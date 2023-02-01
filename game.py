eb7f71aa288bcb1fdd4d2b523683cd4ae50f2fbe
An Bui <anhipchipchip@gmail.com>
2023-02-01 01:22:06 -0500

    sudo code

---
+++ game.py
@@ -0,0 +1,22 @@
+# How to play 
+
+# display welcome message
+# render empty board
+# Decide who moves first X or O 
+# player X takes a turn 
+# check for a winner - func 
+# player O takes a turn - func
+# check for winner
+# after win - display winner - func
+# if no winner - display tie - func
+
+
+# potential state
+# whose turn it is - x/o
+# winner - true/false
+
+# create a start func: 
+# game.takeTurn()
+# game.checkforWin()
+# game.diplayWin()
+

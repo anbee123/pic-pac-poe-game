b7f423b8c0271cdfdd456f8a1ef364808c213c2b
An Bui <anhipchipchip@gmail.com>
2023-02-02 21:09:07 -0500

    created dashboard and orders

--- game.py
+++ game.py
@@ -26,3 +26,25 @@ class PyPacPoe():
     self.player_selection = ""
     self.game_over = False
     self.turns = 0
+    self.current_board = {'a1': " ", 
+                          'b1': " ", 
+                          'c1': " ",
+                          'a2': " ", 
+                          'b2': " ", 
+                          'c2': " ",
+                          'a3': " ", 
+                          'b3': " ", 
+                          'c3': " ",
+                         }
+
+    self.winning_lists = [
+      ['a1','b1','c1'],
+      ['a2','b2','c2'],
+      ['a3','b3','c3'],
+      ['a1','a2','a3'],
+      ['b1','b2','b3'],
+      ['c1','c2','c3'],
+      ['a1','b2','c3'],
+      ['a3','b2','c1']
+    ]
+                     
